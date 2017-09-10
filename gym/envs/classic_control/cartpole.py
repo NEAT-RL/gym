8a524c8fdfbda13a230541bd098f0cf779605666
Harman Singh <harman.singh@hotmail.co.nz>
2017-09-10 23:45:31 +1200

    Revert cartpole env

--- gym/envs/classic_control/cartpole.py
+++ gym/envs/classic_control/cartpole.py
@@ -22,16 +22,16 @@ class CartPoleEnv(gym.Env):
     def __init__(self):
         self.gravity = 9.8
         self.masscart = 1.0
-        self.masspole = 0.15  # m
+        self.masspole = 0.1
         self.total_mass = (self.masspole + self.masscart)
-        self.length = 0.75  # was 0.5 actually half the pole's length. l
+        self.length = 0.5 # actually half the pole's length
         self.polemass_length = (self.masspole * self.length)
         self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
 
         # Angle at which to fail the episode
         self.theta_threshold_radians = 12 * 2 * math.pi / 360
-        self.x_threshold = 10  # upper and lower bound x
+        self.x_threshold = 2.4
 
         # Angle limit set to 2 * theta_threshold_radians so failing observation is still within bounds
         high = np.array([
@@ -49,9 +49,6 @@ class CartPoleEnv(gym.Env):
 
         self.steps_beyond_done = None
 
-        self.time_step = 0.1
-        self.restitution = 0.8
-
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
@@ -61,103 +58,16 @@ class CartPoleEnv(gym.Env):
         state = self.state
         x, x_dot, theta, theta_dot = state
         force = self.force_mag if action==1 else -self.force_mag
-
-        sim_steps = int(math.ceil(self.time_step / 0.01))
-        simu_time = self.time_step / sim_steps
-
-        for i in range(sim_steps):
-            costheta = math.cos(theta)
-            sintheta = math.sin(theta)
-            temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
-            thetaacc = (self.gravity * sintheta - costheta* temp) / ((self.length / 2.0) * ((4.0/3.0) - self.masspole * costheta * costheta / self.total_mass))
-            xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
-
-            # Update the four state variables, using Euler's method.
-            '''
-            x, x_dot, theta, theta_dot = state
-            x == newx
-            x_dot = newdx
-            theta = newtheta
-            theta_dot = newdtheta
-            x = oldX
-            double newx = state.getState()[0];
-            double newdx = state.getState()[1];
-            double newtheta = state.getState()[2];
-            double newdtheta = state.getState()[3];
-            double oldX = newx;
-            '''
-
-
-            '''
-            oldX = newx;
-                newx = newx + simuTime * newdx;
-                newdx = newdx + simuTime * xacc;
-                newtheta = newtheta + simuTime * newdtheta;
-                newdtheta = newdtheta + simuTime * thetaacc;
-            '''
-            old_x = x
-            x = x + simu_time * x_dot
-            x_dot = x_dot + simu_time * xacc
-            theta = theta + simu_time * theta_dot
-            theta_dot = theta_dot + simu_time * thetaacc
-
-            while theta >= math.pi:
-                theta -= 2.0 * math.pi
-
-            while theta < -math.pi:
-                theta += 2.0 * math.pi
-
-
-            '''
-            boolean wasOOB = MathUtils.isInRange(oldX, lowBoundX, uppBoundX);
-            boolean isOOB = MathUtils.isInRange(newx, lowBoundX, uppBoundX);
-
-            if (isOOB && !wasOOB) {
-                newdx *= restitution;
-
-                if (newx > uppBoundX) {
-                    newdx = -Math.abs(newdx);
-                } else if (newx < lowBoundX) {
-                    newdx = Math.abs(newdx);
-                }
-            }
-            '''
-            wasOOB = self._is_in_range(old_x, -self.x_threshold, self.x_threshold)
-            isOOB = self._is_in_range(x, -self.x_threshold, self.x_threshold)
-
-            if isOOB and not wasOOB:
-                x_dot *= self.restitution
-
-                if x > self.x_threshold:
-                    x_dot = -math.fabs(x_dot)
-                elif x < -self.x_threshold:
-                    x_dot = math.fabs(x_dot)
-
-            '''
-            newdx = MathUtils.constrainToBound(newdx, -10, 10);
-            newdtheta = MathUtils.constrainToBound(newdtheta, -10, 10);
-
-            if (isTerminalState(new State(new double[]{newx, newdx, newtheta, newdtheta}))) {
-                //this.notifyTerminalState();
-                //reset();
-                break;
-            }
-            
-            '''
-            x_dot = max(min(x_dot, 10), -10)
-            theta_dot = max(min(theta_dot, 10), -10)
-
-            # done = x < -self.x_threshold \
-            #        or x > self.x_threshold \
-            #        or theta < -self.theta_threshold_radians \
-            #        or theta > self.theta_threshold_radians
-
-            done = math.fabs(x) > self.x_threshold or math.fabs(theta) > 0.2 * math.pi
-            if done:
-                break
-
-        self.state = (x, x_dot, theta, theta_dot)
-
+        costheta = math.cos(theta)
+        sintheta = math.sin(theta)
+        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta* temp) / (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
+        xacc  = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+        x  = x + self.tau * x_dot
+        x_dot = x_dot + self.tau * xacc
+        theta = theta + self.tau * theta_dot
+        theta_dot = theta_dot + self.tau * thetaacc
+        self.state = (x,x_dot,theta,theta_dot)
         done =  x < -self.x_threshold \
                 or x > self.x_threshold \
                 or theta < -self.theta_threshold_radians \
@@ -178,13 +88,8 @@ class CartPoleEnv(gym.Env):
 
         return np.array(self.state), reward, done, {}
 
-    @staticmethod
-    def _is_in_range(value, min_value, max_value):
-        return max_value <= value >= min_value
-
     def _reset(self):
-        # self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
-        self.state = np.asarray([0, 0, -0.15, 0])
+        self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))
         self.steps_beyond_done = None
         return np.array(self.state)
 
@@ -385,10 +290,7 @@ class AdaptedCartPoleEnv(gym.Env):
 
         self.state = (x, x_dot, theta, theta_dot)
 
-        done = x < -self.x_threshold \
-               or x > self.x_threshold \
-               or theta < -self.theta_threshold_radians \
-               or theta > self.theta_threshold_radians
+        done = math.fabs(x) > self.x_threshold or math.fabs(theta) > 0.2 * math.pi
         done = bool(done)
 
         if not done:
